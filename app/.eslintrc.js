0e269f5258805bbf9850608148b3760862b556a2
Dario <darionoucandi@gmail.com>
2025-08-27 23:36:01 +0200

    eslint and gitignore improvements

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -2,21 +2,112 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['@typescript-eslint'],
   extends: [
-    'eslint:recommended',
+    'eslint:recommended'
   ],
   rules: {
+    // TypeScript rules
     'prefer-const': 'error',
     'no-var': 'error',
     'no-unused-vars': 'off', // Turn off base rule
     '@typescript-eslint/no-unused-vars': 'error',
+
+    // Indentation and spacing rules
+    indent: ['error', 2, {
+      SwitchCase: 1,
+      VariableDeclarator: 1,
+      outerIIFEBody: 1,
+      MemberExpression: 1,
+      FunctionDeclaration: { parameters: 1, body: 1 },
+      FunctionExpression: { parameters: 1, body: 1 },
+      CallExpression: { arguments: 1 },
+      ArrayExpression: 1,
+      ObjectExpression: 1,
+      ImportDeclaration: 1,
+      flatTernaryExpressions: false,
+      ignoreComments: false
+    }],
+    'no-tabs': 'error',
+    'no-mixed-spaces-and-tabs': 'error',
+
+    // Spacing rules
+    'space-before-blocks': 'error',
+    'space-before-function-paren': ['error', {
+      anonymous: 'always',
+      named: 'never',
+      asyncArrow: 'always'
+    }],
+    'space-in-parens': ['error', 'never'],
+    'space-infix-ops': 'error',
+    'space-unary-ops': ['error', {
+      words: true,
+      nonwords: false
+    }],
+    'spaced-comment': ['error', 'always', {
+      line: { markers: ['/'], exceptions: ['-', '+'] },
+      block: { markers: ['*'], exceptions: ['*'], balanced: true }
+    }],
+    'key-spacing': ['error', { beforeColon: false, afterColon: true }],
+    'comma-spacing': ['error', { before: false, after: true }],
+    'semi-spacing': ['error', { before: false, after: true }],
+    'object-curly-spacing': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+    'computed-property-spacing': ['error', 'never'],
+
+    // Line breaks and semicolons
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'never'],
+    'eol-last': ['error', 'always'],
+    'no-trailing-spaces': 'error',
+    'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1 }],
+
+    // Quotes and strings
+    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    'quote-props': ['error', 'as-needed'],
+
+    // Function and variable formatting
+    'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    curly: ['error', 'all'],
+    'no-else-return': 'error',
+    'consistent-return': 'error',
+
+    // Import/Export formatting
+    'no-duplicate-imports': 'error',
+    'import/order': 'off', // We'll handle this manually or with prettier
+
+    // Code quality
+    'no-console': 'warn',
+    'no-debugger': 'error',
+    'no-alert': 'error',
+    'no-eval': 'error',
+    'no-implied-eval': 'error',
+    'no-new-func': 'error',
+    'no-script-url': 'error',
+
+    // Best practices
+    eqeqeq: ['error', 'always'],
+    'no-eq-null': 'error',
+    'no-floating-decimal': 'error',
+    'no-implicit-coercion': 'error',
+    'no-multi-spaces': 'error',
+    'no-redeclare': 'error',
+    'no-return-assign': 'error',
+    'no-self-compare': 'error',
+    'no-throw-literal': 'error',
+    'no-unused-expressions': 'error',
+    'no-useless-call': 'error',
+    'no-useless-concat': 'error',
+    'no-void': 'error',
+    radix: 'error',
+    'wrap-iife': ['error', 'outside'],
+    yoda: 'error'
   },
   env: {
     node: true,
     es6: true,
-    jest: true,
-  },
+    jest: true
+  }
 };
